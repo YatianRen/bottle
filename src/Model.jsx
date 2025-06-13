9f66dad6a3b0c3c79f8e39a95093ed5b05c2c027
YatianRen <92707026+YatianRen@users.noreply.github.com>
2025-06-12 17:09:02 -0700

    Updated Model.jsx

--- src/Model.jsx
+++ src/Model.jsx
@@ -226,7 +226,7 @@ const Model = (props) => {
         ref={modelRef}
         // ROTATION: [x-axis, y-axis, z-axis] in radians
         // Current: slightly rotated to show the bottle nicely
-        rotation={[-96, 1.2, 0]}
+        rotation={[-97, 1.2, 0]}
         
         // POSITION: [x, y, z] - move the bottle in 3D space
         // x: left(-) / right(+)
